b3c2eecb010e782613ba1c5819ed4e36b098992e
Lucasm4sco <lmbp202@gmail.com>
2022-11-03 20:32:27 -0300

    atividade day 11 challenge

---
+++ HackerRank/30DaysOfCode/day11-binaryNumbers.js
@@ -0,0 +1,31 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+
+
+function main() {
+    const n = parseInt(readLine().trim(), 10);
+    
+    const numbers = n.toString(2).split('0');
+    const sorted = numbers.sort((a, b) => a - b );
+    console.log(sorted[sorted.length - 1].length)
+}
